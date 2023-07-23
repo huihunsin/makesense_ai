f43c78904124e0ba9040a204a29ed4d257ae7f3c
huihun sin <139689233+huihunsin@users.noreply.github.com>
2023-07-23 16:40:03 +0900

    add_category_id
    When you use makesense.ai to label data,
    sometimes it said
    "Annotation import was unsuccessful
    Cannot read properties of undefined (reading 'id') "
    Maybe you didn't label it or you got an error when saving.
    So I made a file for error correction.

---
+++ add_category_id.py
@@ -0,0 +1,17 @@
+from json import load, dump
+
+id = int(input("Enter the number of category_id you want to label: "))
+
+with open('error.json',) as f:
+    data = load(f)
+
+annotation_data = data["annotations"]
+
+for i in annotation_data:
+    if i.get("category_id", False):
+        i["category_id"] = id
+
+data["annotations"] = annotation_data
+
+with open('modified.json', 'w', encoding='utf-8') as f:
+    dump(data, f)
